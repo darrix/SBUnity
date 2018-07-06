46296dadbdd5a1cd4ceb65620e8fea4eaf0f39f9
darrix <dmhendricks@gmail.com>
2018-07-06 14:01:28 -0400

    Added notes

---
+++ Assets/Notes/notes.md
@@ -0,0 +1,4 @@
+# Unity samples
+
+Created this and didn't have a sample camera or lighting setup.  Why?
+
---
+++ Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 2017.2.0f3  version
+ * 
+ */
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// action taken on script load
+	    Debug.Log("Hello, world");
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// action taken with every frame (what controls how quickly this is evaluated?)
+	}
+}
